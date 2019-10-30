b1ac95834c77c9c8b6e943397054b054422e5e2a
Timothy Logan <timtimmahh@gmail.com>
2019-10-30 15:42:52 -0400

    Reorganized project

--- ztnet/cmake-build-debug/include/libzt/CMakeFiles/miniupnpc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "lib/libminiupnpc.pdb"
-  "lib/libminiupnpc.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/miniupnpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- ztnet/cmake-build-debug/include/libzt/CMakeFiles/natpmp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "lib/libnatpmp.pdb"
-  "lib/libnatpmp.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/natpmp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
